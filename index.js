07688710c6a4cb5d1ff265b7f7545848a2babf17
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-04-22 18:40:30 +0100

    New

--- index.js
+++ index.js
@@ -1,76 +1,70 @@
+// Variable declarations
 let playerSelection;
 let computerSelection;
 
 let playerScore = 0;
 let computerScore = 0;
 
+let count = 0 // Takes note of the game round.
+
 const myArray = ["rock", "paper", "scissors"];
 
-function game(){
-    checkWinner();
-    return playGame();
-    // for(let i = 0; i < 5; i++){    
-    // }   
-}
-
-//Check if the winner has reached three.
-function checkWinner(){
-    if(playerScore == 3){
-        alert("Game over, you win");
-    }
-    else if (computerScore == 3){
-        alert("Na wa oooh");
-    }    
-};
-
-
+// Allows the computer make a random choice
 function computerPlay() {
   return myArray[~~(Math.random() * myArray.length)];
 }
 
-function playerPlay(){
-    let input = prompt("rock,paper or scissors").toLowerCase();
-    return input;
+//Allow the make an input
+function playerPlay() {
+  let input = prompt("rock,paper or scissors").toLowerCase();
+  return input;
 }
 
+// //Check if the winner has reached three.
+function checkWinner() {
+    count ++
+    if(count === 5){
+        if(playerScore == 3){
+        alert("Game over, you win");
+        }
+        else if (computerScore == 3){
+        alert("Na wa oooh");
+        }
+        else{
+        alert("It's a general draw")
+        }
+    }
+}
 
-function playGame(){
-    
-    playerSelection = playerPlay();
-    computerSelection = computerPlay();
+// Plays a round
+function playRound() {
+  playerSelection = playerPlay();
+  computerSelection = computerPlay();
 
-    //Check game conditions
-    if(playerSelection ==="rock" && computerSelection ==="paper"){
-        playerScore++;
-        return "Player wins"
-        
-    }
-    else if (playerSelection === computerSelection){
-        return "It is a draw! play again";
-    }
-    else if(playerSelection ==="paper" && computerSelection ==="rock"){
-        computerScore ++;
-        return "Computer wins"
-    }
-    else if(playerSelection==="rock" && computerSelection==="scissors"){
-        playerScore ++;
-        return "Player wins"
-    }
-
-    else if(playerSelection==="scissors" && computerSelection==="rock"){
-        computerScore ++;
-        return "Computer wins"
-    }
-    else if(playerSelection==="scissors" && computerSelection==="paper"){
-        playerScore ++;
-        return "Player wins"
-    }
-    else if(playerSelection==="paper" && computerSelection==="scissors"){
-        computerScore ++;
-        return "Computer wins"
-    }
-    else{
-        return playerPlay();
-    }
-
+  // check winner
+  checkWinner();
+  //Check game conditions
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    playerScore++;
+    return "Player wins";
+  } else if (playerSelection === computerSelection) {
+    return "It is a draw! play again";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    computerScore++;
+    return "Computer wins";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    return "Player wins";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
+    return "Computer wins";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    return "Player wins";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
+    return "Computer wins";
+  } else {
+    return playerPlay();
+  }
 }
