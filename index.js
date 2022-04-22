9cfdf3da558e6b337953777634a9bba6e03eb67c
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-04-22 18:13:52 +0100

    New update

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
-// let playerSelection;
-// let computerSelection;
+let playerSelection;
+let computerSelection;
 
-// let playerScore = 0;
-// let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-// const myArray = ["rock", "paper", "scissors"];
+const myArray = ["rock", "paper", "scissors"];
 
-// function game(){
+function game(){
+    checkWinner();
+    return playGame();
+    // for(let i = 0; i < 5; i++){    
+    // }   
+}
 
-//     checkWinner();
-//     for(let i = 0; i < 5; i++){
-//         return playGame();
-//     }
-
-//     //Check if the winner has reached three.
-// }
-
-// function checkWinner(){
-//     if(playerScore == 3){
-//         alert("Game over, you win");
-//     }
-//     else if (computerScore == 3){
-//         alert("Na wa oooh");
-//     }    
-// };
+//Check if the winner has reached three.
+function checkWinner(){
+    if(playerScore == 3){
+        alert("Game over, you win");
+    }
+    else if (computerScore == 3){
+        alert("Na wa oooh");
+    }    
+};
 
 
-// function computerPlay() {
-//   return myArray[~~(Math.random() * myArray.length)];
-// }
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
+}
 
-// function playerPlay(){
-//     let input = prompt("rock,paper or scissors").toLowerCase();
-//     return input;
-// }
+function playerPlay(){
+    let input = prompt("rock,paper or scissors").toLowerCase();
+    return input;
+}
 
 
-// function playGame(){
+function playGame(){
     
-//     playerSelection = playerPlay();
-//     computerSelection = computerPlay();
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
 
-//     //Check game conditions
-//     if(playerSelection ==="rock" && computerSelection ==="paper"){
-//         playerScore++;
-//         return "Player wins"
+    //Check game conditions
+    if(playerSelection ==="rock" && computerSelection ==="paper"){
+        playerScore++;
+        return "Player wins"
         
-//     }
-//     else if (playerSelection === computerSelection){
-//         return "It is a draw! play again";
-//     }
-//     else if(playerSelection ==="paper" && computerSelection ==="rock"){
-//         computerScore ++;
-//         return "Computer wins"
-//     }
-//     else if(playerSelection==="rock" && computerSelection==="scissors"){
-//         playerScore ++;
-//         return "Player wins"
-//     }
+    }
+    else if (playerSelection === computerSelection){
+        return "It is a draw! play again";
+    }
+    else if(playerSelection ==="paper" && computerSelection ==="rock"){
+        computerScore ++;
+        return "Computer wins"
+    }
+    else if(playerSelection==="rock" && computerSelection==="scissors"){
+        playerScore ++;
+        return "Player wins"
+    }
 
-//     else if(playerSelection==="scissors" && computerSelection==="rock"){
-//         computerScore ++;
-//         return "Computer wins"
-//     }
-//     else if(playerSelection==="scissors" && computerSelection==="paper"){
-//         playerScore ++;
-//         return "Player wins"
-//     }
-//     else if(playerSelection==="paper" && computerSelection==="scissors"){
-//         computerScore ++;
-//         return "Computer wins"
-//     }
-//     else{
-//         return playerPlay();
-//     }
+    else if(playerSelection==="scissors" && computerSelection==="rock"){
+        computerScore ++;
+        return "Computer wins"
+    }
+    else if(playerSelection==="scissors" && computerSelection==="paper"){
+        playerScore ++;
+        return "Player wins"
+    }
+    else if(playerSelection==="paper" && computerSelection==="scissors"){
+        computerScore ++;
+        return "Computer wins"
+    }
+    else{
+        return playerPlay();
+    }
 
-// }
+}
--- prapra.js
+++ prapra.js
@@ -14,3 +14,80 @@
 //         Show the choice : You chose this while computer chose this
 //         Add one to winner
 //         Update game round 
+
+// let playerSelection;
+// let computerSelection;
+
+// let playerScore = 0;
+// let computerScore = 0;
+
+// const myArray = ["rock", "paper", "scissors"];
+
+// function game(){
+//     checkWinner();
+//     return playGame();
+//     // for(let i = 0; i < 5; i++){    
+//     // }   
+// }
+
+// //Check if the winner has reached three.
+// function checkWinner(){
+//     if(playerScore == 3){
+//         alert("Game over, you win");
+//     }
+//     else if (computerScore == 3){
+//         alert("Na wa oooh");
+//     }    
+// };
+
+
+// function computerPlay() {
+//   return myArray[~~(Math.random() * myArray.length)];
+// }
+
+// function playerPlay(){
+//     let input = prompt("rock,paper or scissors").toLowerCase();
+//     return input;
+// }
+
+
+// function playGame(){
+    
+//     playerSelection = playerPlay();
+//     computerSelection = computerPlay();
+
+//     //Check game conditions
+//     if(playerSelection ==="rock" && computerSelection ==="paper"){
+//         playerScore++;
+//         return "Player wins"
+        
+//     }
+//     else if (playerSelection === computerSelection){
+//         return "It is a draw! play again";
+//     }
+//     else if(playerSelection ==="paper" && computerSelection ==="rock"){
+//         computerScore ++;
+//         return "Computer wins"
+//     }
+//     else if(playerSelection==="rock" && computerSelection==="scissors"){
+//         playerScore ++;
+//         return "Player wins"
+//     }
+
+//     else if(playerSelection==="scissors" && computerSelection==="rock"){
+//         computerScore ++;
+//         return "Computer wins"
+//     }
+//     else if(playerSelection==="scissors" && computerSelection==="paper"){
+//         playerScore ++;
+//         return "Player wins"
+//     }
+//     else if(playerSelection==="paper" && computerSelection==="scissors"){
+//         computerScore ++;
+//         return "Computer wins"
+//     }
+//     else{
+//         return playerPlay();
+//     }
+
+// }
